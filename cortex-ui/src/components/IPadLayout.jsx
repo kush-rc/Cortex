bf4d757c634679b6a936e0701e94515e72d285dc
Kush Chhunchha <kushchhunchha@gmail.com>
2026-03-07 16:35:23 +0530

    fix: resolve case-sensitive import error breaking linux build

--- cortex-ui/src/components/IPadLayout.jsx
+++ cortex-ui/src/components/IPadLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
-import './iPadLayout.css'
+import './IPadLayout.css'
 
 const IPadLayout = ({ products }) => {
     // Correct Order: Pro, Air, iPad, mini
